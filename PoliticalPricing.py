27e1a4e01e07f55c0e5e18ae64b37946a3ed1e00
Jesús Miguel Martínez Camrena <miguel.martinez@ciencias.unam.mx>
2018-12-24 19:08:53 -0600

    Update PoliticalPricing.py

--- PoliticalPricing.py
+++ PoliticalPricing.py
@@ -3,42 +3,15 @@
 """
 Created on Thu Nov 29 19:38:34 2018
 
-This sheet is thought to be an example on how to use DTM for a particular case. Problem is stated as follows. 
+This sheet is thought to be an example on how to use DTM for a particular case. 
 
-The goverment has told a monopoly company it must reduce prices by 10% over the course
+Problem set is proposed in pictures within the repositories
 
 @author: Chino
 """
 
 from scipy.misc import derivative
 from scipy.integrate import quad
-
-def recursiveY(k, gamma, alpha, beta):
-    """
-    
-    """
-    if (0 == k):
-        return [1]            #Y(0)=y(0)=1
-    elif (1 == k):
-        return [1, gamma]
-    elif (2 == k):
-        return [1, gamma, 1/(4*(3*beta*gamma-alpha))]
-    elif (2 < k):
-        result = 0
-        r = k-2     #r stated in (3)
-        previous = recursiveY(k-1, alpha, beta, gamma)
-        delta = 2*(r+1)*(r+2)*(3*beta*gamma-alpha)
-        addTerm = r*previous[r]*gamma
-        addTerm = addTerm - 2*r*(r+1)*previous[r+1]*previous[2]
-        summatory = 0
-        for m in range(r-1):        #range goes up to one number before
-            summatory += (m+1)*(m+2)*previous[m+2]*(r-m-1)*previous[r-m-1]
-            summatory += (m+1)*(r-m)*previous[m+1]*previous[r-m]
-            summatory -= (m+1)*(m+2)*previous[m+2]*(r-m+1)*previous[r-m+1]
-        result += 6*beta*(summatory + addTerm)
-        result /= delta
-        previous.append(result)
-        return previous
 
 def P(t, y, alpha, beta):
     """
@@ -84,3 +57,30 @@ def integLag(y, alpha, beta):
         alpha,beta: a float
     """
     return quad(Lag,0,1,args=(y,alpha,beta))
+
+def recursiveY(k, gamma, alpha, beta):
+    """
+    
+    """
+    if (0 == k):
+        return [1]            #Y(0)=y(0)=1
+    elif (1 == k):
+        return [1, gamma]
+    elif (2 == k):
+        return [1, gamma, 1/(4*(3*beta*gamma-alpha))]
+    elif (2 < k):
+        result = 0
+        r = k-2     #r stated in (3)
+        previous = recursiveY(k-1, alpha, beta, gamma)
+        delta = 2*(r+1)*(r+2)*(3*beta*gamma-alpha)
+        addTerm = r*previous[r]*gamma
+        addTerm = addTerm - 2*r*(r+1)*previous[r+1]*previous[2]
+        summatory = 0
+        for m in range(r-1):        #range goes up to one number before
+            summatory += (m+1)*(m+2)*previous[m+2]*(r-m-1)*previous[r-m-1]
+            summatory += (m+1)*(r-m)*previous[m+1]*previous[r-m]
+            summatory -= (m+1)*(m+2)*previous[m+2]*(r-m+1)*previous[r-m+1]
+        result += 6*beta*(summatory + addTerm)
+        result /= delta
+        previous.append(result)
+        return previous
